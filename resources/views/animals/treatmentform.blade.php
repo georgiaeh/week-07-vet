1bbc5b9c3dda856249b5376410194c8bb0869da5
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-02-26 14:22:52 +0000

    Drop down for adding treatments to animals

--- resources/views/animals/treatmentform.blade.php
+++ resources/views/animals/treatmentform.blade.php
@@ -22,18 +22,24 @@
             </div>
 
             <div class="form-group">
-                <label for="treatment">Treatment Name</label>
-                <input id="treatment" name="treatment" 
-                    class="form-control 
-                    @error('treatment') is-invalid @enderror" 
-                    {{-- value="{{ old('name') === null ? $animal->name ?? ''  : old('name') }}" --}}
-                />
-                @error('treatment')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
+                <label for="treatment">Treatment Name: </label>
+                <br>
+                <select class="custom-select mr-sm-2"  id="treatment" name="treatment">
+                    <option value="Pecti-Cap">Pecti-Cap</option>
+                    <option value="Zymox Ear Cleanser ">Zymox Ear Cleanser </option>
+                    <option value="Veda-Sorb Bolus">Veda-Sorb Bolus</option>
+                    <option value="Keto-Gel ">Keto-Gel </option>
+                    <option value="QuickDerm Wound Ointment ">QuickDerm Wound Ointment </option>
+                    <option value="Gener-Lyte"> Gener-Lyte   </option>
+                    <option value="Good-boi shots"> Good-boi shots   </option>
+                    <option value="Good-boi shots"> Tooth Cleaning</option>
+                    <option value="Grooming"> Grooming  </option>
+                    <option value="Belly Rubs"> Belly Rubs</option>
+                    <option value="Ear Scratches"> Ear Scratches</option>
+                    <option value="Hugs"> Hugs</option>
+                    <option value="Tail Wiggler"> Tail Wiggler </option>
+                  </select>
+            </div> 
         </fieldset>
 
         <div class="card-footer text-right">
